329896899637af986e7c4d7f694b6ac98a5cbcd8
smith <smithbabawande@gmail.com>
2023-10-03 10:00:55 +0100

    Completed Assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return " and ".join(items)
+    elif len(items) > 2:
+        return ", ".join(items[:-1]) + ", and " + items[-1]
+    else:
+        return None
